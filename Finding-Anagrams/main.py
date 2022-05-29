f649278cec44f1ed886c1b22221f09c20c0f2b81
Obumbillions <obumnemeokoy914@gmail.com>
2022-05-29 17:28:10 +0100

    code indentation

--- Finding-Anagrams/main.py
+++ Finding-Anagrams/main.py
@@ -4,8 +4,15 @@
 # find_anagrams("below", "elbow") --> True
 
 
-def find_anagram(word, anagram):
+def find_anagram(word1, word2):
     # [assignment] Add your code here
 
-    return True
+    if len(word1) == len(word2) and sorted(word1.lower()) == sorted(word2.lower()):
+        return True
 
+    else:
+        return False
+
+word1 = input('word1: ')
+word2 = input('word2: ')
+print("--->", find_anagram(word1, word2))
